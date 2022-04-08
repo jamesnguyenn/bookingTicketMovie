ca8ac687a0b2319ec1593dc002c4c56f8b77ac98
James Nguyen <66525324+jamesnguyenn@users.noreply.github.com>
2022-04-08 17:12:07 +0700

    change height index

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import BillLists from "./components/BillLists";
 export default class App extends Component {
   render() {
     return (
-      <div className="w-full lg:h-[100vh] relative text-white px-2 py-5 flex gap-[20px] lg:flex-row flex-col items-start justify-center">
-        <div className="absolute w-full h-full inset-0 bg-black bg-opacity-60 overlay z-[-1]" />
+      <div className="w-full min-h-screen relative text-white px-2 py-5 flex gap-[20px] lg:flex-row flex-col items-start justify-center">
+        <div className="absolute w-full h-full inset-0  bg-black bg-opacity-60 overlay z-[-1]" />
         {/* Đặt vé */}
-        <div className="lg:w-[60%] lag:h-full w-[100%] h-[auto] flex flex-col justify-center items-center gap-[10px] z-[10]">
+        <div className="lg:w-[60%] h-[full] w-[100%] flex flex-col justify-center items-center gap-[10px] z-[10]">
           <h1 className="w-full text-center text-[3.5rem] font-bold text-[#ffc107] uppercase">
             Đặt vé xem phim
           </h1>
@@ -19,7 +19,7 @@ export default class App extends Component {
           <ChairLists />
         </div>
         {/* Thông tin đặt vé xem phim */}
-        <div className="lg:w-[40%] lg:h-full w-full h-[auto] flex flex-col justify-center items-center lg:gap-[40px] gap:[20px] z-[10] px-5 ">
+        <div className="lg:w-[40%] h-full w-full flex flex-col justify-center items-center lg:gap-[40px] gap:[20px] z-[10] px-5 ">
           {/* Thông tin hiển thị ghế */}
           <ChairInfo />
           {/* Thông tin hóa đơn */}
